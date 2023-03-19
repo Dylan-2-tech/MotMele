c45584bf82342f9358494eaa1cde371ea71f9f5a
Dylan <dylanhoungbedji@gmail.com>
2023-03-19 23:07:10 +0100

    Premier test sur les direction mais la je suis fatigué j'arrive pas à réfléchir

--- ClassGame.py
+++ ClassGame.py
@@ -2,58 +2,57 @@ from tkinter import *
 from tkinter import ttk
 import tkinter.font as font
 
-# Constante des différentes directions
-DIAGONALE_GD = "diagonale_gauche_droite"
-DIAGONALE_DG = "diagonale_droite_gauche"
-LIGNE_HORIZONTAL = "ligne_horizontale"
-LIGNE_VERTICAL = "ligne_verticale"
-
 
 #### Fichier des class du jeu ####
 
-# Class De la fenetre de jeu
-class Jeu:
+# Class de la fenetre de jeu
+class Jeu(Tk):
 
 	def __init__(self):
-		# C'est la map qui contiendras les lettres
-		self.gameMap = [ 
-					["P","B","G","N","I","Z","Y","I"],
-					["B","H","Y","A","W","Q","U","A"],
-					["H","R","D","T","P","O","V","W"],
-					["L","J","G","Z","I","M","R","F"],
-					["E","X","B","N","K","W","T","Y"],
-					["X","T","S","L","G","Y","H","O"],
-					["V","J","A","R","O","Y","F","P"],
-					["P","M","H","E","I","M","A","U"]]
+		super().__init__()
 
+		self.gameMap = [ 
+					["P","O","U","R","I","Z","Y","I"],
+					["B","H","S","A","W","Q","U","A"],
+					["H","R","I","T","P","O","V","W"],
+					["L","J","R","R","I","M","R","F"],
+					["E","X","I","N","K","W","T","Y"],
+					["X","O","S","L","G","Y","H","O"],
+					["V","J","A","R","O","Y","F","P"],
+					["P","M","H","E","I","M","A","U"]
+					]
+		# Méthode qui renvoie la map du jeu
+		def get_map(self):
+			return self.gameMap
 
 		# Initialisation de la fenetre du jeu
-		self.gameWindow = Tk()
-		self.gameWindow.title("Mot Croisé") # Titre du jeu
-		self.gameWindow.geometry("1200x650+400+250") # Dimmension de la fenetre
-		self.gameWindow.minsize(width = 1200, height = 650) # Dimmension minimum de la fenetre
-		self.gameWindow.maxsize(width = 1200, height = 650) # Dimmension maximale de la fenetre
-		self.gameWindow.configure(bg="#45458B")
+		self.title("Mot Mélé") # Titre du jeu
+		self.geometry("1200x650+400+250") # Dimmension de la fenetre
+		self.minsize(width = 1200, height = 650) # Dimmension minimum de la fenetre
+		self.maxsize(width = 1200, height = 650) # Dimmension maximale de la fenetre
+		self.configure(bg="#45458B")
 
 		# Boutton pour valider la sélection des lettres
-		self.valideBtn = Button(self.gameWindow, text = "Valider", bg = "green", fg = "white")
+		self.valideBtn = Button(self, text = "Valider", bg = "green", fg = "white", command = lambda:valider(self))
 		self.valideBtn.place(x = 800, y = 150)
 
-
 		# Boutton pour quitter la partie
-		self.leaveBtn = Button(self.gameWindow,text="Quitter",bg = "red",fg = "white",command=self.gameWindow.destroy, font = font.Font(size=15))
+		self.leaveBtn = Button(self,text="Quitter",bg = "red",fg = "white",command=self.destroy, font = font.Font(size=15))
 		self.leaveBtn.place(x = 900, y = 420) # emplacement forcé sur des pixel précis
 
-
 		# Label qui affiche le mot valider
-		self.MotLabel = Label(self.gameWindow,text = "")
-
+		self.MotLabel = Label(self,bg = "#45458B",font = font.Font(size=20),fg = "white")
+		self.MotLabel.place(x = 700, y = 300)
 
 		# Mot qui va changer au fur et à mesure de la partie
-		self.mot = Mot(self.MotLabel)
+		self.mot = Mot()
+
+		# Méthode Valider
+		def valider(self):
+			self.MotLabel.configure(text = self.mot.mot_correct())
 
 
-		# génération de la map
+		# génération de la map des lettres
 		posy = 40 # position y du boutton de la lettre
 		for x in range(len(self.gameMap)): # on parcours la map en x
 			posx = 40 # La position initiale de chaque boutton en x est donc 0 pour la premiere ligne
@@ -64,20 +63,41 @@ class Jeu:
 				posx+=75 # On incrémente de 100 la position en x pour laisser quelques pixels pour d'écart en horizontal
 			posy += 75 # On incrémente de 115 la position en y pour laisser un espace de quelques pixels d'écart en vertical
 
+		self.mainloop() # Affichage de la fenetre
 
+		"""
+		
 		# Le bouton qui va deséléctionné toutes les lettres
 		self.ClearLettersBtn = Button(self.gameWindow,text = "Clear", command = None)
 		self.ClearLettersBtn.place(x = 800, y = 420)
 
+
+		# Fonction Valider
+		def Valider(self):
+			print(self.mot)
+
+
+		# Fonction qui nettoie les lettres sélectionner mais pas validé
 		def clear(self):
 			for listeLettre in self.gameMap:
 				for lettre in listeLettre:
-					if lettre.isClicked:
+					if lettre.isClicked and not lettre.valide:
 						lettre.isClicked = False
 						lettre.boutton.confgure(bg = "#9090EE",activebackground="#A3A3FE")
 
+		def check_Word(self):
+			i = 0
+			trouve = false
+			while (not trouve and i < len(self.listeMots)):
+				trouve = listeMots[i] == self.mot.text.afficher_Text()
+				i += 1
 
-		self.gameWindow.mainloop()
+			if trouve:
+				print("Mot trouve")
+			else:
+				print("Mot pas trouve")
+
+		"""
 
 
 # Class des lettres
@@ -85,75 +105,28 @@ class Lettre:
 
 	def __init__(self,lettre,x,y,game):
 
-		self.index = -1
 		self.mot = game.mot
-		self.gameWindow = game.gameWindow
-		self.gameMap = game.gameMap # la map où apparaissent les lettre
 		self.isClicked = False # Si il est cliqué alors
 		self.x = x # position x de la lettre
 		self.y = y # position y de la lettre
 		self.lettre = lettre
-		self.boutton = Button(self.gameWindow,text="".join(lettre),
+		self.boutton = Button(game,text="".join(lettre),
 			width = 3, height = 1, font = font.Font(size=20),
 			bg="#9090EE", activebackground="#A3A3FE", bd=0,
 			command=self.clicked)
-
-
+	
 	def clicked(self): # méthode qui s'active quand la lettre est cliqué
 
 		if self.isClicked: # Si la lettre est déja cliqué
-			self.boutton.configure(bg = "#9090EE",activebackground="#A3A3FE")
-			self.isClicked = False
-			self.mot.mot = [self.mot.mot[i] for i in range(len(self.mot.mot)) if self != self.mot.mot[i]]
+			self.boutton.configure(bg = "#9090EE",activebackground="#A3A3FE") # On remet une couleur par défaut 
+			self.isClicked = False # Elle devient plus cliqué
+			self.mot.text = [self.mot.text[i] for i in range(len(self.mot.text)) if self != self.mot.text[i]] # On retire la lettre du mot
 		
 		else: # Si la lettre n'est pas encore cliqué
-			# Si la lettre à droite est cliqué
-			if self.x+1 < len(self.gameMap[0]) and self.gameMap[self.x+1][self.y].isClicked:
-				self.mot.ajouterLettre(self)
-				self.index = len(self.mot.mot)-1
-				self.boutton.configure(bg = "red",activebackground="red")
-				self.isClicked = True
-				print(f"la lettre {self.lettre} est cliqué en haut de la lettre {self.gameMap[self.x+1][self.y].lettre}")
-
-			# Si la lettre en bas est cliqué par rapport à celle de droite
-			elif self.x-1 > 0 and self.gameMap[self.x-1][self.y].isClicked:
-				self.mot.ajouterLettre(self)
-				self.index = len(self.mot.mot)-1
-				self.boutton.configure(bg = "red",activebackground="red")
-				self.isClicked = True
-				print(f"la lettre {self.lettre} est cliqué en bas de la lettre {self.gameMap[self.x-1][self.y].lettre}")
-
-			# Si la lettre à gauche est cliqué par rapport à celle de droite 
-			elif self.y+1 < len(self.gameMap) and self.gameMap[self.x][self.y+1].isClicked:
-				self.mot.ajouterLettre(self)
-				self.index = len(self.mot.mot)-1
-				self.boutton.configure(bg = "red",activebackground="red")
-				self.isClicked = True
-				print(f"la lettre {self.lettre} est cliqué à gauche de la lettre {self.gameMap[self.x][self.y+1].lettre}")
-
-			# Si la lettre à droite est cliqué par rapport à celle de gauche
-			elif self.y-1 > 0 and self.gameMap[self.x][self.y-1].isClicked:
-				self.mot.ajouterLettre(self)
-				self.index = len(self.mot.mot)-1
-				self.boutton.configure(bg = "red",activebackground="red")
-				self.isClicked = True
-				print(f"la lettre {self.lettre} est cliqué à droite de {self.gameMap[self.x][self.y-1].lettre}")
-
-			else:
-				self.mot.ajouterLettre(self)
-				self.boutton.configure(bg = "red",activebackground="red")
-				self.isClicked = True
-				
-
-			# Si la lettre en haut à gauche est cliqué
-			# Si la lettre en haut à droite est cliqué
-			# Si la lettre en bas ç gauche est cliqué
-			# Si la lettre en bas à droite est cliqué
-
-		print(f"le mot est {self.mot.afficherMot()}")
-
-
-
+			self.boutton.configure(bg = "red",activebackground="red") # On change sa couleur pour dire qu'elle est cliqué
+			self.isClicked = True # Elle devient cliqué
+			self.mot.ajouter_Lettre(self) # On l'ajoute au mot
+ 	
 	"""
 	Comment faire en sorte que le joueur ne clique pas sur des lettres impossible à choisir
 
@@ -194,18 +167,89 @@ class Lettre:
 # Class du mot qui sera créé au fur et à mesure
 class Mot:
 
-	def __init__(self,displayLabel):
-		self.mot = []
-		self.displayLabel = displayLabel
-		self.direction = None
+	def __init__(self):
+		self.text = []
 
-	def ajouterLettre(self, lettre):
-		self.mot.append(lettre)
+	def __str__(self):
+		return "".join(self.text[i].lettre for i in range(len(self.text)))
 
-	def afficherMot(self):
-		return "".join(self.mot[i].lettre for i in range(len(self.mot)))
-		#self.displayLabel.set(self.mot)
+	def ajouter_Lettre(self, lettre):
+		self.text.append(lettre)
 
+	def mot_correct(self):
+		"""Renvoie True si chaque lettre du mot suivent la même direction"""
+
+		if len(self.text) > 1:
+
+			startX = self.text[0].x+1 # Position x pour comparer les positions avec self.text[i].x
+			startY = self.text[0].y+1 # Position y pour comparer les positions avec self.text[i].y
+			indLetter = 1 # Indice de départ pour comparais les lettres
+			wellFormed = False # Booléen qui va confirmé si le mot est mal formé
+
+			# Si toutes les lettres sont positionné horizontalement
+			while not wellFormed and indLetter < len(self.text):
+				wellFormed = self.text[indLetter].x == startX and self.text[indLetter].y == startY
+				if indLetter == len(self.text)-1:
+					return f"mot horizontal {indLetter}"
+				else:
+					startX += 1
+					indLetter += 1
+
+			if wellFormed:
+				return "Mot horizontal"
+			else:
+				return "mot pas horizontale"
+
+		else:
+			return "Pas un mot"
+
+
+		"""
+		# Si le mot est bien formé horizontalement
+		# On réinitialise les valeurs de comparaisons
+		startX = self.text[0].x+1 # Position x pour comparer les positions avec self.text[i].x
+		indLetter = 1 # Indice de départ pour comparais les lettres
+
+		# Si toutes les lettres sont positionné verticalement
+		while not wellFormed and indLetter < len(self.text): 
+			if self.text[indLetter].y == startY: # Si le mot est bien formé verticalement
+				wellFormed = True
+				return True
+			else:
+				startY += 1
+				indLetter += 1
+		# On réinitialise les valeurs de comparaisons
+		startX = self.text[0].x+1 # Position x pour comparer les positions avec self.text[i].x
+		startY = self.text[0].y+1 # Position y pour comparer les positions avec self.text[i].y
+		indLetter = 1 # Indice de départ pour comparais les lettres
+
+		# Si toutes les lettres sont positionné en diagonale d'en haut à gauche à en bas à droite
+		while not wellFormed and indLetter < len(self.text):
+			if self.text[indLetter].x == startX and self.text[indLetter].y == startY: # Si le mot est bien formé horizontalement alors on renvoie True
+				wellFormed = True
+				return True
+			else:
+				startX += 1
+				startY += 1
+				indLetter += 1
+		# On réinitialise les valeurs de comparaisons
+		startX = self.text[0].x-1 # Position x pour comparer les positions avec self.text[i].x
+		startY = self.text[0].y+1 # Position y pour comparer les positions avec self.text[i].y
+		indLetter = 1 # Indice de départ pour comparais les lettres
+
+		# Si les lettres sont positionné toutes en diagonale d'en haut à droite à en bas à gauche
+		while not wellFormed and indLetter < len(self.text):
+			if self.text[indLetter].x == startX and self.text[indLetter].y == startY: # Si le mot est bien formé horizontalement alors on renvoie True
+				wellFormed = True
+				return True
+			else:
+				startX -= 1
+				startY += 1
+				indLetter += 1
+		
+		return wellFormed
+		"""
+		
 
 	"""
 	La direction du mot est diagonale gauche droite si
@@ -227,4 +271,47 @@ class Mot:
 	on peut cliquer sur la lettre choisis et que ses coordonnées sont
 	self.mot[len(mot)-1].x+1 et self.mot[len(mot)-1].y
 	ou self.mot[len(mot)-1].x-1 et self.mot[len(mot)-1].y
+
+
+			# Si la lettre à droite est cliqué
+			if self.x+1 < len(self.gameMap[0]) and self.gameMap[self.x+1][self.y].isClicked:
+				self.mot.ajouter_Lettre(self)
+				self.index = len(self.mot.text)-1
+				self.boutton.configure(bg = "red",activebackground="red")
+				self.isClicked = True
+				print(f"la lettre {self.lettre} est cliqué en haut de la lettre {self.gameMap[self.x+1][self.y].lettre}")
+
+			# Si la lettre en bas est cliqué par rapport à celle de droite
+			elif self.x-1 > 0 and self.gameMap[self.x-1][self.y].isClicked:
+				self.mot.ajouter_Lettre(self)
+				self.index = len(self.mot.text)-1
+				self.boutton.configure(bg = "red",activebackground="red")
+				self.isClicked = True
+				print(f"la lettre {self.lettre} est cliqué en bas de la lettre {self.gameMap[self.x-1][self.y].lettre}")
+
+			# Si la lettre à gauche est cliqué par rapport à celle de droite 
+			elif self.y+1 < len(self.gameMap) and self.gameMap[self.x][self.y+1].isClicked:
+				self.mot.ajouter_Lettre(self)
+				self.index = len(self.mot.text)-1
+				self.boutton.configure(bg = "red",activebackground="red")
+				self.isClicked = True
+				print(f"la lettre {self.lettre} est cliqué à gauche de la lettre {self.gameMap[self.x][self.y+1].lettre}")
+
+			# Si la lettre à droite est cliqué par rapport à celle de gauche
+			elif self.y-1 > 0 and self.gameMap[self.x][self.y-1].isClicked:
+				self.mot.ajouter_Lettre(self)
+				self.index = len(self.mot.text)-1
+				self.boutton.configure(bg = "red",activebackground="red")
+				self.isClicked = True
+				print(f"la lettre {self.lettre} est cliqué à droite de {self.gameMap[self.x][self.y-1].lettre}")
+
+			else:
+				self.mot.ajouter_Lettre(self)
+				self.boutton.configure(bg = "red",activebackground="red")
+				self.isClicked = True
+
+			# Si la lettre en haut à gauche est cliqué
+			# Si la lettre en haut à droite est cliqué
+			# Si la lettre en bas ç gauche est cliqué
+			# Si la lettre en bas à droite est cliqué
 	"""
--- motMele.py
+++ motMele.py
@@ -1,7 +1,13 @@
-from tkinter import *
-from tkinter import ttk
-import tkinter.font as font
 from ClassGame import *
 
+"""
+text = [1,2,4,45,6]
+
+
+ind = 1
+while ind < len(text):
+	print(ind, len(text))
+	ind += 1
+"""
 
 jeu = Jeu()
