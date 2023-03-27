3ab6eb1b5494764ac49e8f05fc2d7f095c4bcc7a
dylan <dylanhoungbedji@gmail.com>
2023-03-27 10:14:23 +0200

    début du tuning du jeu

--- motMele.py
+++ motMele.py
@@ -2,4 +2,13 @@
 from ClassGame import *
 import glob
 
+"""
+liste_de_grille = glob.glob("grille/*.txt")
+
+for i in range(len(liste_de_grille)):
+	liste_de_grille[i] = liste_de_grille[i].replace("grille/",'')
+	liste_de_grille[i] = liste_de_grille[i].replace('.txt','')
+
+print(liste_de_grille)
+"""
 Jeu = Menu()
