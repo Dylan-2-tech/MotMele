13b424f97e3e8aece54a4cf57853a9268c158bce
Dylan <dylanhoungbedji@gmail.com>
2023-05-01 18:38:27 +0200

    Début de l'algorithme de création de grille

--- algo_grille.py
+++ algo_grille.py
@@ -30,13 +30,99 @@ def affichage_grille(g):
 
 	for listeLettre in grille:
 		print(listeLettre)
+	print()
 
 grille = [["" for i in range(9)] for i in range(9)]
 liste_mot = ["manger","siffler","caillou"]
 
+grille[3][6] = 'T'
 affichage_grille(grille)
 
-def recursive_search_vert_gd(grille, taille):
-	
 
-def placer_mot_vertical_gd(grille,mot):
+# Fonction qui place un mot donner à la verticale
+def search_vert(mot, x, y):
+	liste_positions = []
+	indC = 0
+
+	while grille[y][x] == "" and indC < len(mot):
+		liste_positions.append((x,y))
+		indC += 1
+		if y < len(grille)-1:
+			y += 1
+
+	if indC == len(mot):
+		placer_mot(mot, liste_positions)
+		return True
+	else:
+		return False
+# Fonction qui place un mot donner à l'horizontale
+def search_hori(mot, x, y):
+	liste_positions = []
+	indC = 0
+
+	while grille[y][x] == "" and indC < len(mot):
+		liste_positions.append((x,y))
+		indC += 1
+		if x < len(grille[0])-1:
+			x += 1
+
+
+	if indC == len(mot):
+		placer_mot(mot, liste_positions)
+		return True
+	else:
+		return False
+# Fonction qui place un mot donner en diagonale de gauche à droite
+def search_diag_gd(mot, x, y):
+	liste_positions = []
+	indC = 0
+
+	while grille[y][x] == "" and indC < len(mot):
+		liste_positions.append((x,y))
+		indC += 1
+		if y < len(grille)-1 and x < len(grille[0])-1:
+			y += 1
+			x += 1
+
+	if indC == len(mot):
+		placer_mot(mot, liste_positions)
+		return True
+	else:
+		return False
+# Fonction qui place un mot donner en diagonale de droite à gauche
+def search_diag_dg(mot, x, y):
+	liste_positions = []
+	indC = 0
+
+	while grille[y][x] == "" and indC < len(mot):
+		liste_positions.append((x,y))
+		indC += 1
+		if y < len(grille)-1 and x > 0:
+			y += 1
+			x -= 1
+
+	if indC == len(mot):
+		placer_mot(mot, liste_positions)
+		return True
+	else:
+		return False
+# Fonction pour placer les lettres du mot au positions voulu
+def placer_mot(mot, liste_positions):
+	# On parcours les position et on place chaque lettre du mot
+	print(liste_positions)
+
+	for i in range(len(liste_positions)):
+		# Récupération des positions
+		x = liste_positions[i][0]
+		y = liste_positions[i][1]
+
+		# Affectation des lettres du mot dans les cases vides de la grille
+		grille[y][x] = mot[i]
+
+print(search_diag_gd("manger", 0, 0))
+#print(search_hori("oui", 1, 2))
+affichage_grille(grille)
+
+'''
+REVERSE D'UNE CHAINE DE CARACTERE: [::-1]
+'''
